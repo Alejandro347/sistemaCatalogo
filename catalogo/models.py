0582f0d5614d4b3ad42743c33ed2e7de1acf0548
Alejandro347 <alemonsan05@gmail.com>
2024-01-11 22:10:50 -0600

    Add Vehiculo model and form, and create Vehiculo creation view

--- catalogo/models.py
+++ catalogo/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Vehiculo(models.Model):
+    marca = models.CharField(max_length=50, blank=False, default="")
     modelo = models.CharField(max_length=50, blank=False)
     categoria = models.CharField(max_length=50, blank=False)
     estado = models.CharField(max_length=50, blank=False)
@@ -11,9 +12,14 @@ class Vehiculo(models.Model):
     transmision = models.CharField(max_length=50, blank=False)
     kilometraje = models.IntegerField(blank=False, default=0)
     
+    def __str__(self):
+        return self.marca + ' - ' + self.modelo
+    
 class Factura(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
     fechaInicio = models.DateField(blank=False)
     fechaFin = models.DateField(blank=False)
     auto = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
     cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.precio) + ' - ' + self.auto.modelo + ' - ' + self.cliente.username
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Vehiculo
+from .forms import VehiculoForm
 
 # Create your views here.
+def crearVehiculo(request):
+    if request.method == 'POST':
+        form = VehiculoForm(request.POST)
+        if form.is_valid():
+            nuevoVehiculo = form.save(commit=False)
+            nuevoVehiculo.save()
+            return render(
+                request,'catalogo/crearVehiculo.html/',
+                {'form': form})
+    else:
+        form = VehiculoForm()
+    return render(request, 'catalogo/crearVehiculo.html/', {'form': form})
